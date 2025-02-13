851ea267f7381a1f8de8edd85eb14ea321b0946b
danji45 <118186664+danji45@users.noreply.github.com>
2025-02-13 03:25:05 +0100

    laatste push voor inleveren WAFS

--- main.js
+++ main.js
@@ -1,11 +1,45 @@
-const response = await fetch('https://fdnd.directus.app/items/person/?filter={%22id%22:197}')
-const data = await response.json()
-console.log(data)
 
 
-const template = document.querySelector('template')
-const result = document.querySelector('#result')
-const engine = new liquidjs.Liquid()
+//  #api importeren# //
+const baseURL = 'https://fdnd.directus.app/';
+const endpointMe = 'items/person/197';
+ 
+const myURL = baseURL + endpointMe;
 
-engine.parseAndRender(template.innerHTML, {persons: data.data}).then(html => result.innerHTML = html)
+async function getData(URL) {
+    return fetch(URL)
+        .then(response => response.json())
+        .then(jsonData => jsonData.data); // Zorg dat we direct bij de 'data' eigenschap komen
+
+}
+
+// #api importeren# //
+// naam //
+
+getData(myURL).then(data => {
+    console.log(data);
+    const greetingElement = document.getElementById("greeting");
+    greetingElement.textContent = `Good Morning, ${data.data.name}!`;
+});
+
+// avatar //
+getData(myURL).then(data => {
+    console.log(data);
+    const avatarImg = document.getElementById("avatar");
+    avatarImg.src = data.data.avatar;
+});
+
+
+async /*9*/ function getData(URL) {
+    return ( //8
+        fetch(URL) //1
+            .then( //2
+                response /*3*/ => response.json() //4
+            )
+            .then( //5
+                jsonData /*6*/ => { return jsonData } //7
+            )
+    );
+}
+
 
