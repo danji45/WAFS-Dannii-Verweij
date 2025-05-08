dd28e37963b3a5abc06e8149a2ef6bc65fda8abf
danji45 <118186664+danji45@users.noreply.github.com>
2025-05-08 11:14:00 +0200

    Inleveren

--- API_Oefenen/obj/Debug/net8.0/API_Oefenen.AssemblyInfo.cs
+++ API_Oefenen/obj/Debug/net8.0/API_Oefenen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Oefenen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+851ea267f7381a1f8de8edd85eb14ea321b0946b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a53b58960b7b5d8791e4e8bbcffb0ee42c504cdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Oefenen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Oefenen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
